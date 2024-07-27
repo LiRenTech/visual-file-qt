98355d720c8cead2e9ed0641defe922f6028fd16
littlefean <2028140990@qq.com>
2024-07-27 18:22:56 +0800

    fix: rutubet继续优化算法

--- entity/entity_folder.py
+++ entity/entity_folder.py
@@ -282,7 +282,7 @@ class EntityFolder(Entity):
         # 调整当前文件夹里的所有实体顺序位置
 
         rectangle_list = [child.body_shape for child in folder.children]
-        sorted_rectangle_list = sort_rectangle(
+        sorted_rectangle_list = sort_rectangle_7_24(
             [rectangle.clone() for rectangle in rectangle_list], self.PADDING
         )
 
@@ -314,6 +314,23 @@ class EntityFolder(Entity):
     pass
 
 
+"""
+装箱问题，排序矩形
+    :param rectangles: N个矩形的大小和位置
+    :param margin: 矩形之间的间隔（为了美观考虑）
+    :return: 调整好后的N个矩形的大小和位置，数组内每个矩形一一对应。
+    例如：
+    rectangles = [Rectangle(NumberVector(0, 0), 10, 10), Rectangle(NumberVector(10, 10), 1, 1)]
+    这两个矩形对角放，外套矩形空隙面积过大，空间浪费，需要调整位置。
+
+    调整后返回：
+
+    [Rectangle(NumberVector(0, 0), 10, 10), Rectangle(NumberVector(12, 0), 1, 1)]
+    参数 margin = 2
+    横向放置，减少了空间浪费。
+"""
+
+
 def sort_rectangle_just_vertical(
     rectangles: list[Rectangle], margin: float
 ) -> list[Rectangle]:
@@ -333,23 +350,109 @@ def sort_rectangle_just_vertical(
 
     return rectangles
 
+def sort_rectangle_7_24(
+    rectangles: list[Rectangle], margin: float
+) -> list[Rectangle]:
+    
+    def append_right(
+        origin: Rectangle, rect: Rectangle, rects: list[Rectangle]
+    ) -> Rectangle:
+        ret = Rectangle(
+            NumberVector(rect.location_left_top.x, rect.location_left_top.y),
+            rect.width,
+            rect.height,
+        )
+        ret.location_left_top.x = origin.right() + margin
+        ret.location_left_top.y = origin.top()
+        # 碰撞检测
+        collision = True
+        while collision:
+            collision = False
+            for r in rects:
+                if ret.is_collision(r):
+                    ret.location_left_top.y = r.bottom() + margin
+                    collision = True
+                    break
+        return ret
 
-def sort_rectangle(rectangles: list[Rectangle], margin: float) -> list[Rectangle]:
-    """
-    装箱问题，排序矩形
-    :param rectangles: N个矩形的大小和位置
-    :param margin: 矩形之间的间隔（为了美观考虑）
-    :return: 调整好后的N个矩形的大小和位置，数组内每个矩形一一对应。
-    例如：
-    rectangles = [Rectangle(NumberVector(0, 0), 10, 10), Rectangle(NumberVector(10, 10), 1, 1)]
-    这两个矩形对角放，外套矩形空隙面积过大，空间浪费，需要调整位置。
+    def append_bottom(
+        origin: Rectangle, rect: Rectangle, rects: list[Rectangle]
+    ) -> Rectangle:
+        ret = Rectangle(
+            NumberVector(rect.location_left_top.x, rect.location_left_top.y),
+            rect.width,
+            rect.height,
+        )
+        ret.location_left_top.y = origin.bottom() + margin
+        ret.location_left_top.x = origin.left()
+        # 碰撞检测
+        collision = True
+        while collision:
+            collision = False
+            for r in rects:
+                if ret.is_collision(r):
+                    ret.location_left_top.x = r.right() + margin
+                    collision = True
+                    break
+        return ret
 
-    调整后返回：
+    rectangles[0].location_left_top.x = 0
+    rectangles[0].location_left_top.y = 0
+    ret = [rectangles[0]]
+    width = rectangles[0].width
+    height = rectangles[0].height
+    # index = 0
+    for i in range(1, len(rectangles)):
+        # if width < height:
+        #     rectangles[i] = append_right(rectangles[index], rectangles[i], ret)
+        #     w = rectangles[i].right()
+        #     if w > width:
+        #         width = w
+        #         index = i
+        # else:
+        #     rectangles[i] = append_bottom(rectangles[index], rectangles[i], ret)
+        #     h = rectangles[i].bottom()
+        #     if h > height:
+        #         height = h
+        #         index = i
+        # ret.append(rectangles[i])
+        min_space_score = -1
+        min_shape_score = -1
+        min_rect = None
+        for j in range(len(ret)):
+            r = append_right(ret[j], rectangles[i], ret)
+            space_score = r.right() - width + r.bottom() - height
+            shape_score = abs(max(r.right(), width) - max(r.bottom(), height))
+            if (
+                min_space_score == -1
+                or space_score < min_space_score
+                or (space_score == min_space_score and shape_score < min_shape_score)
+            ):
+                min_space_score = space_score
+                min_shape_score = shape_score
+                min_rect = r
+            r = append_bottom(ret[j], rectangles[i], ret)
+            space_score = r.right() - width + r.bottom() - height
+            shape_score = abs(max(r.right(), width) - max(r.bottom(), height))
+            if (
+                min_space_score == -1
+                or space_score < min_space_score
+                or (space_score == min_space_score and shape_score < min_shape_score)
+            ):
+                min_space_score = space_score
+                min_shape_score = shape_score
+                min_rect = r
+        width = max(width, r.right())
+        height = max(height, r.bottom())
+        ret.append(min_rect)
 
-    [Rectangle(NumberVector(0, 0), 10, 10), Rectangle(NumberVector(12, 0), 1, 1)]
-    参数 margin = 2
-    横向放置，减少了空间浪费。
-    """
+    return ret
+
+
+def sort_rectangle_right_bottom(
+    rectangles: list[Rectangle], margin: float
+) -> list[Rectangle]:
+    """ """
 
     def append_right(
         origin: Rectangle, rect: Rectangle, rects: list[Rectangle]
